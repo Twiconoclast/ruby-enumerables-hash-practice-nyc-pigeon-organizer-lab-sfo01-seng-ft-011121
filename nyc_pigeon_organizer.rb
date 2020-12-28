14454ef22865aaa6a8eca11dd63139ea64e9c548
Carrie Bares <caroltbares@gmail.com>
2020-12-28 20:57:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  by_name = {}
+  data.each do |cgl, cgl_hash|
+    cgl_hash.each do |cgl_s, name_arr|
+      name_arr.each do |name|
+        new_arr = (cgl_hash.select {|k, v| v.include?(name)}).keys
+        new_arr = new_arr.map {|k| k.to_s}
+        by_name[name]= {cgl => new_arr}
+        by_name[name][cgl] = new_arr
+      end
+    end
+  end
+  return by_name
 end
